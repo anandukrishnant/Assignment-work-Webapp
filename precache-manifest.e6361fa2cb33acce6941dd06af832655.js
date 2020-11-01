46b2cdbea0f3ec7d8ebc15a9cae565f156864658
anandukrishnant <anandukrishnan100@gmail.com>
2020-11-01 22:53:43 +0530

    Updates

---
+++ precache-manifest.e6361fa2cb33acce6941dd06af832655.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a305765d375bfde6035d83d11ac8fea",
+    "url": "/Assignment-work-Webapp/index.html"
+  },
+  {
+    "revision": "35d5b4ff277a858a31bd",
+    "url": "/Assignment-work-Webapp/static/css/main.8a0cedcb.chunk.css"
+  },
+  {
+    "revision": "dbbf3404f255aec07c4f",
+    "url": "/Assignment-work-Webapp/static/js/2.ffb5456d.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Assignment-work-Webapp/static/js/2.ffb5456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d5b4ff277a858a31bd",
+    "url": "/Assignment-work-Webapp/static/js/main.b2989230.chunk.js"
+  },
+  {
+    "revision": "2ce8fe82c261fcc4f5c8",
+    "url": "/Assignment-work-Webapp/static/js/runtime-main.bfea7f4a.js"
+  }
+]);
